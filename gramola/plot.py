3af731fbebc10989d86ac28abe3e32f056bf0ce4
pfreixes <pfreixes@gmail.com>
2016-02-03 00:10:20 +0100

    Get the terminal size via a code get from the console module

--- gramola/plot.py
+++ gramola/plot.py
@@ -50,7 +50,8 @@ class Plot(object):
         self.__drawn = False
 
     def width(self):
-        return int(os.environ.get('COLUMNS', 80))
+        width, _ = getTerminalSize()
+        return width
 
     def draw(self, datapoints):
         """ Render using the the datapoints given as a parameters, Gramola
@@ -85,3 +86,27 @@ class Plot(object):
         sys.stdout.write("min={}, max={}, last={}".format(min(values), max(values), values[-1]))
         sys.stdout.flush()
         self.__drawn = True
+
+
+# Code get from the console module
+def getTerminalSize():
+    env = os.environ
+    def ioctl_GWINSZ(fd):
+        try:
+            import fcntl, termios, struct, os
+            cr = struct.unpack('hh', fcntl.ioctl(fd, termios.TIOCGWINSZ,
+        '1234'))
+        except:
+            return
+        return cr
+    cr = ioctl_GWINSZ(0) or ioctl_GWINSZ(1) or ioctl_GWINSZ(2)
+    if not cr:
+        try:
+            fd = os.open(os.ctermid(), os.O_RDONLY)
+            cr = ioctl_GWINSZ(fd)
+            os.close(fd)
+        except:
+            pass
+    if not cr:
+        cr = (env.get('LINES', 25), env.get('COLUMNS', 80))
+    return int(cr[1]), int(cr[0])
